59c3d716fd44646da1675676dd8e02eec3f49786
main Trieu <trieu.nguyenxuan@hcmut.edu.vn>
2025-04-03 13:29:12 +0700

    commit to another pc

--- component/c_circuit_work/cutting/width_c.py
+++ component/c_circuit_work/cutting/width_c.py
@@ -13,19 +13,42 @@ from qiskit.quantum_info import SparsePauliOp
 class Result_of_cutting:
     sub_circuits: list[QuantumCircuit]
     overhead: float # The overhead of the circuit cutting
-
-def gate_to_reduce_width(qc: QuantumCircuit, cutname: str) -> dict[QuantumCircuit]:
-    observable = SparsePauliOp(["ZZII", "IZZI", "-IIZZ", "XIXI", "ZIZZ", "IXIX"])  
-    partitioned_problem = partition_problem(
-        circuit= qc, partition_labels="AAAAABBB", observables=observable.paulis
-    )
-    subcircuits = partitioned_problem.subcircuits
-    subobservables = partitioned_problem.subobservables
-    bases = partitioned_problem.bases
-    overhead = np.prod([basis.overhead for basis in bases])
     
-    return subcircuits 
-      
+def has_measurement(circuit: QuantumCircuit) -> bool:
+    # Iterate over all instructions in the circuit
+    for instr in circuit.data:
+        if instr.operation.name == 'measure':  # Check if the operation name is 'measure'
+            return True
+    return False
+
+def create_string(num_char, num_dif, position: list):
+    """
+    """
+    # Initialize the string with the first character 'A'
+    result = ['A'] * num_char
+    print(type(result))
+    print(result)  
+    # Generate the different characters 'A', 'B', 'C', ..., based on num_dif
+    char_list = [chr(ord('A') + i) for i in range(num_dif)]
+    print(char_list)
+    
+    # Modify the positions to introduce new characters
+    count = 0
+    print(len(position))
+    for i in range(len(result)):
+        print(type(i))
+        print(i)
+        if (i) == position[count]:
+            count += 1
+        result[i] = char_list[count]
+    
+    # Convert the list back to a string and return
+    return ''.join(result)
+
+# Truyen vao circuit, cat o vi tri nao
+def gate_to_reduce_width(qc: QuantumCircuit, cut_position: list(list(int))) -> dict[QuantumCircuit]:
+
+    pass
 
 def wire_to_reduce_width():
     # Devide the circuit into many parts following the depth
