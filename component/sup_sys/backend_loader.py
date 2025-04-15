24ca248cf48775d7ee92c8baa27cd9f54cc86b7e
main Trieu <trieu.nguyenxuan@hcmut.edu.vn>
2025-04-15 15:24:37 +0700

    Refactor code structure for improved readability and maintainability

--- component/sup_sys/backend_loader.py
+++ component/sup_sys/backend_loader.py
@@ -1,10 +1,8 @@
 from component.a_backend import fake_backend
+import pandas as pd
 
 def load_backends():
     imported_names = dir(fake_backend)
-    backends = {
-        name: getattr(fake_backend, name).num_qubits
-        for name in imported_names
-        if not name.startswith('__') and hasattr(getattr(fake_backend, name), 'num_qubits')
-    }
-    return backends
+    backends = [name for name in imported_names if not name.startswith('__')]
+    backend_qubits = {name: getattr(fake_backend, name)().num_qubits for name in backends}
+    return backend_qubits
