df7a9d62b63488986fd419ba9aec6030b97031b2
main Trieu <trieu.nguyenxuan@hcmut.edu.vn>
2025-05-22 09:02:56 +0700

    update automation script

--- automation_script_run.sh
+++ automation_script_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # chmod +x automation_script_run.sh
-# files=("runLoopTestFFD.py" "runLoopTestMTMC.py" "runLoopTestNoTaDS.py" "runLoopTestMILQ.py")
+files=("runLoopTestFFD.py" "runLoopTestMTMC.py" "runLoopTestNoTaDS.py" "runLoopTestMILQ.py")
 # files=("runLoopTestNoTaDS.py" "runLoopTestMILQ.py")
-files=("runLoopTestNoTaDS.py")
+# files=("runLoopTestNoTaDS.py")
 for file in "${files[@]}"
 do
-  for argv1 in {2..3}
+  for argv1 in {2..19}
   do
     for argv2 in {2..10}
     do
